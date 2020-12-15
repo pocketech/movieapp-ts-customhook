c1a48a379ee358c7f3e48fc699d0849632d13d53
Kento Nakano <hakuyowasedatech@gmail.com>
2020-12-15 12:24:48 +0900

    shrink tailwindconfig

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -10,8 +10,7 @@ const DEFAULT_PLACEHOLDER_IMAGE =
 
 const MovieCard: React.FC<Props> = ({ movie }) =>
   <div className="relative flex flex-col justify-between w-48 p-4 overflow-hidden text-center duration-200 ease-in-out bg-white border-t-2 border-b-2 border-yellow-300 rounded-lg shadow-lg h-80">
-    <h2 className="overflow-hidden text-xl font-bold cursor-pointer overflow-ellipsis whitespace-nowrap" tooltip="">{movie.Title}</h2>
-
+    <h2 className="overflow-hidden text-xl font-bold cursor-pointer overflow-ellipsis whitespace-nowrap">{movie.Title}</h2>
     <img
       className="block object-contain max-h-52"
       src={movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster}
